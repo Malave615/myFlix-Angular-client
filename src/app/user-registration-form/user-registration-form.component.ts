24de84b1f4144f3489ecad31b0e03297fceefc04
Malave615 <malavetracy@gmail.com>
2025-03-12 17:20:08 -0400

    Reverting previous changes.

--- src/app/user-registration-form/user-registration-form.component.ts
+++ src/app/user-registration-form/user-registration-form.component.ts
@@ -25,22 +25,19 @@ export class UserRegistrationFormComponent implements OnInit {
 
   // Function sending the form inputs to the backend
   registerUser(): void {
-    this.fetchApiData.userRegistration(this.userData).subscribe({
-      next: (response) => {
+    this.fetchApiData.userRegistration(this.userData).subscribe((response) => {
       // Logic for a successful user registration goes here! (To be implemented)
       this.dialogRef.close(); // This will close the modal on success!
       console.log(response);
       this.snackBar.open('User registered successfully!', 'OK', {
         duration: 2000
       });
-    }, 
-    error: (errorResponse) => {
-      console.error(errorResponse);
-      const errorMessage = errorResponse.error?.message || 'An error occurred during registration.';
-      this.snackBar.open(errorResponse, 'OK', {
+    }, (response) => {
+      console.log(response);
+      this.snackBar.open(response, 'OK', {
         duration: 2000
       });
     }
-  });
+  );
 }
 }
