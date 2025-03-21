a3ceec6890091dde773ceb7c94969b28b28c8efa
Malave615 <malavetracy@gmail.com>
2025-03-21 15:11:57 -0400

    Added routing pages.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'welcome', component: WelcomePageComponent },
+  // { path: 'movies', component: MovieCardComponent },
+ // { path: 'profile', component: ProfileComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
